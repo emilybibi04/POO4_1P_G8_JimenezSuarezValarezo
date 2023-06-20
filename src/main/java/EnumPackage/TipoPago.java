ac8049abda80b50886b83921db06eee11dc76a57
Emily Valarezo <136937944+emilybibi04@users.noreply.github.com>
2023-06-20 09:41:32 -0500

    Cambio de nombres

--- src/main/java/EnumPackage/TipoPago.java
+++ src/main/java/EnumPackage/TipoPago.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
  */
-package SistemaVehicular;
+package EnumPackage;
 
 /**
  *
  * @author emilyvalarezo
  */
-public enum Perfil {
-    CLIENTE, OPERADOR
+public enum TipoPago {
+    EFECTIVO, CRÉDITO
 }
---
+++ src/main/java/MainPackage/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainPackage;
+
+/**
+ *
+ * @author emilyvalarezo
+ */
+public class Main {
+    public static void main(String[] args){
+        
+    }
+}
