ac3919ef648cae0d7f4cb516767616cc9227957a
gsus25 <jedasuar@espol.edu.ec>
2023-07-09 12:37:15 -0500

    Sobreescritura del metodo opcionesMenu en la clase Operador

--- src/main/java/MainPackage/Operador.java
+++ src/main/java/MainPackage/Operador.java
@@ -5,6 +5,10 @@
 
 package MainPackage;
 import EnumPackage.Perfil;
+import static MainPackage.SistemaVehicular.usuariosRegistrados;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 
 /**
  *
@@ -17,15 +21,63 @@ import EnumPackage.Perfil;
 
 public class Operador extends Usuario {
     //atributo de instancia de 'Operador'
-     private double sueldo;
+    private double sueldo;
      
-     public Operador(String cedula, String nombres, String apellidos, int edad, String correo, String usuario, String contrasena, Perfil perfil, double sueldo){
-         //invocación del constructor de la clase padre
-         super(cedula, nombres, apellidos, edad, correo, usuario, contrasena, perfil);
+    public Operador(String cedula, String nombres, String apellidos, int edad, String correo, String usuario, String contrasena, Perfil perfil, double sueldo){
+        //invocación del constructor de la clase padre
+        super(cedula, nombres, apellidos, edad, correo, usuario, contrasena, perfil);
+        this.sueldo=sueldo;
      }
      
-     @Override
-     public void opcionesMenu(){
-         System.out.println("pipi");
-     }
+    public double getSueldo(){
+       return sueldo;
+    }
+    //sobreescritura del metodo opcionesMenu
+    @Override
+    public void opcionesMenu() {
+    usuariosRegistrados = Utilitaria.cargarUsuario();
+    Scanner sc = new Scanner(System.in);
+    int opcion;
+
+    // Opciones para el Usuario Cliente
+    do {
+        System.out.println("--------------------------------------------------");
+        System.out.printf("%32s", "OPCIONES OPERADOR");
+        System.out.println("\n--------------------------------------------------");
+        System.out.println("1. Registrar Pagos");
+        System.out.println("2. Consultar multas clientes");
+        System.out.println("3. Consultar usuarios");
+        System.out.println("4. Salir");
+        System.out.print("Ingrese el número de la opción que desea: ");
+
+        try {
+            opcion = sc.nextInt();
+            sc.nextLine();
+            if (opcion < 1 || opcion > 4) { //validar que solo ingrese numeros del 1-4
+                System.out.println("Opción inválida. Por favor, selecciona una opción válida.");
+            } else {
+                switch (opcion) {
+                    case 1:
+                        registrarPagos();
+                        break;
+                    case 2:
+                        consultarMultas();
+                        break;
+                    case 3:
+                        consultarUsuarios(usuariosRegistrados);
+                        break;
+                    case 4:
+                        System.out.println("Saliendo del programa...");
+                        break;
+                }
+            }
+        } catch (InputMismatchException e) { //validar que no ingrese caracteres diferentes de numeros
+            System.out.println("Opción inválida. Por favor, ingrese un número válido.");
+            sc.nextLine(); // Limpiar el búfer
+            opcion = 0; // Establecer la opción en 0 para repetir el ciclo
+        }
+    } while (opcion != 4);
+}
+     
+     
 }
